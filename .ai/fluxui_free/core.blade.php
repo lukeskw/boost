36b03f1c38e2680dd044ef3ee5dd1c28135ed786
Ashley Hindle <ashley.hindle@laravel.com>
2025-07-27 08:45:11 -0600

    guidelines: improve flux

--- .ai/fluxui_free/core.blade.php
+++ .ai/fluxui_free/core.blade.php
@@ -1,3 +1,8 @@
-- **Use Flux UI components** when available
+- Flux UI is a component library for Livewire. Flux is a robust, hand-crafted, UI component library for your Livewire applications. It's built using Tailwind CSS and provides a set of components that are easy to use and customize.
+- You should use Flux UI components when available.
+- Flux UI components look like this:
+<code-snippet>
+    <flux:button variant="primary"/>
+</code-snippet>
 - Fallback to standard Blade components if Flux is unavailable.
 - Use Laravel Boost's `search-docs` tool to get the exact documentation & code snippets available for this project.
