c8956d54684f8686355c55431770797cf2314dec
Ashley Hindle <ashley.hindle@laravel.com>
2025-08-05 14:42:58 +0100

    guidelines: more forceful herd guidelines

--- .ai/herd/core.blade.php
+++ .ai/herd/core.blade.php
@@ -1,2 +1,5 @@
-- This site is hosted with Laravel Herd. You MUST NOT ever run `artisan serve`.
 - The site is always available at: {{ config('app.url') }}
+- We do not need to run any commands to make the site available. It is always available through Herd at: {{ config('app.url') }}
+
+- This site is hosted with Laravel Herd. You MUST NOT ever run `artisan serve`.
+- You MUST NOT EVER run `php artisan serve`
