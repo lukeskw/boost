35246f20bed19b4d3455bc9cc2de08c47b45a785
Mateus Guimarães <mateus.jatene@gmail.com>
2025-08-07 17:32:36 -0300

    add failing test

---
+++ tests/Feature/Middleware/InjectBoostTest.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Feature\Middleware;
+
+use Illuminate\Support\Facades\Route;
+use Laravel\Boost\Middleware\InjectBoost;
+use Tests\TestCase;
+
+class InjectBoostTest extends TestCase
+{
+    public function test_it_maintains_the_original_view_response_type(): void
+    {
+        $this->app['view']->addNamespace('test', __DIR__.'/../../fixtures');
+
+        Route::get('injection-test', function () {
+            return view('test::injection-test');
+        })->middleware(InjectBoost::class);
+
+        $response = $this->get('injection-test');
+
+        $response->assertViewIs('test::injection-test');
+    }
+}
