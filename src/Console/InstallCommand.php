9a7eb9b3ef38aa3d583102f77f188caf37f364b3
Ashley Hindle <ashley.hindle@laravel.com>
2025-08-28 12:57:19 +0100

    refactor: phpstan for displayhelper::grid

--- src/Console/InstallCommand.php
+++ src/Console/InstallCommand.php
@@ -390,7 +390,7 @@ class InstallCommand extends Command
 
         $this->newLine();
         $this->info(sprintf(' Adding %d guidelines to your selected agents', $guidelines->count()));
-        DisplayHelper::grid($guidelines->map(fn ($guideline, $key) => $key.($guideline['custom'] ? '*' : ''))->sort()->toArray(), $this->terminal->cols());
+        DisplayHelper::grid($guidelines->map(fn ($guideline, string $key) => $key.($guideline['custom'] ? '*' : ''))->values()->sort()->toArray(), $this->terminal->cols());
         $this->newLine();
         usleep(750000);
 
